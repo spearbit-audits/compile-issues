4aa7204ab04a6962df1ac738d9f0adc9be05a022
Emanuele Ricci <stermi@gmail.com>
2022-02-15 19:53:54 +0100

    updated code to handle multiple labels
    - Only open issues are fetched
    - Only 1 severity label is allowed per issue

--- compile.py
+++ compile.py
@@ -15,23 +15,25 @@ import config
 REPO = config.REPO
 github = Github(config.TOKEN)
 
-issue_dict = {}
+SEVERITY_LABELS = ['Severity: Critical Risk', 'Severity: High Risk', 'Severity: Medium Risk', 'Severity: Low Risk', 'Severity: Gas Optimization', 'Severity: Informational']
+
+issue_dict : dict[str, list[str]] = {}
 
 # TODO catch get_repo() 404 errors and produce a gentle suggestion on what's wrong.
-
+# "GitHub's REST API v3 considers every pull request an issue"--need to filter them out.
 for issue in github.get_repo(REPO).get_issues():
-    # TODO what happens when there are more than one issue labels. In that
-    # case, need to put the issue into the label with highest severity.
-    if len(issue.labels) != 0:
+    if issue.state == 'open' and issue.pull_request is None:
+        # filter issue labels for only severity labels
+        severity_labels_in_issue = [label.name for label in issue.labels if label.name in SEVERITY_LABELS]
+
+        assert len(severity_labels_in_issue) == 1, f"Issue {issue.html_url} has more than one (or no) severity label."
         label = issue.labels[0].name
         if label not in issue_dict:
-            issue_dict[label] = []
+           issue_dict[label] = []
         issue_dict[label].append(f"### {issue.title} \n{issue.body}\n")
 
-labels = ['Severity: Critical Risk', 'Severity: High Risk', 'Severity: Medium Risk', 'Severity: Low Risk', 'Severity: Gas Optimization', 'Severity: Informational']
-
 with open("report.md", "w") as report:
-    for label in labels:
+    for label in SEVERITY_LABELS:
         report.write(f"## {label[10:]}\n\n")
         for content in issue_dict[label]:
             report.write(content.replace("\r\n", "\n"))
